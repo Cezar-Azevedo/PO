c0e50c2b48b59a69b09c86e90cd520db729ad399
Cezar-Azevedo <cezar_di_azevedo@yahoo.com.br>
2020-10-07 22:52:55 -0300

    Delete 10.0-Somatorio_fracoes.py

--- 10.0-Somatorio_fracoes.py
+++
@@ -1,8 +0,0 @@
-def somatorio(n):
-    if(n==1):
-        return 1/4
-    else:
-        return somatorio(n-1) + ((2*n-1)/((-2)**(n+1)))
-
-n = int(input("Digite um número para somar: "))
-print ("Resultado:", somatorio(n))
